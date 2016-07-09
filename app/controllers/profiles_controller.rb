be839491d0f0da0fc06400c51aa7220e56a953bd
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-07-09 17:32:51 +0900

    add profile post function

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,16 @@
 class ProfilesController < ApplicationController
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.create(profile_params)
+    redirect_to :root
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:age, :job, :comment).merge(user_id: current_user.id)
+  end
+
 end
