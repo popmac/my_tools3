40a628ec1d1091704406886cecfd91c660aefcfe
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-07-21 19:25:43 +0900

    fix javascript setting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
+// require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
