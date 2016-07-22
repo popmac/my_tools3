84118c5b656388fafddee07b422bd95af16bd3ab
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-07-22 18:04:05 +0900

    enable login for emai_adress or nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+        :validatable, :authentication_keys => [:login]
   has_one :profile
   has_many :photos
 
   before_create :build_default_profile
+
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["nickname = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
 
   private
   def build_default_profile
