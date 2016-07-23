9a1203ee3c228a9b142ce3290a535053ee9834ae
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-07-23 16:29:19 +0900

    enable signup, add email params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :nickname
+    devise_parameter_sanitizer.for(:sign_up) << [:nickname ,:email]
   end
 
 end
