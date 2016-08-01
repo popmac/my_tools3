01c656ef3e456a1fe0c364d4f7698da0732fd50d
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-01 16:30:58 +0900

    model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710043906) do
+ActiveRecord::Schema.define(version: 20160801072840) do
 
   create_table "photos", force: :cascade do |t|
     t.string   "image",      limit: 255
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20160710043906) do
     t.string   "job",        limit: 255
     t.text     "comment",    limit: 65535
     t.integer  "user_id",    limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tools", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
