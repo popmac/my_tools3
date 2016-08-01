02c1b01932eff713bc38d57b86822d99c4f246b4
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-01 17:08:03 +0900

    add auto_complete_tool_name at routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
     resources :photos
   end
   resources :profiles
-  resources :tools
+  resources :tools do
+    collection do
+      get 'autocomplete_tool_name'
+    end
+  end
   resources :reviews
   root 'users#index'
 end
