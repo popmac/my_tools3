de649f47333dbac4ad4a06ece85ba0f8a725763a
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-01 18:46:48 +0900

    add tool and review post function

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,3 +1,37 @@
 class ToolsController < ApplicationController
   autocomplete :tool, :name, :full => true
+
+  def new
+    @tool = Tool.new
+    @review = Review.new
+    # @tool.reviews.build
+  end
+
+  def create
+    @tool = Tool.where(name: params[:tool][:name]).first_or_initialize
+    if @tool.name.present?
+      @tool.save
+    else
+      render 'new'
+      flash[:notice] = "アプリとレビューを両方とも入力してください"
+      return
+    end
+    @review = Review.new(review_params)
+    if @review.review.present?
+      @review.save
+      redirect_to users_path
+      flash[:notice] = "アプリとレビューを登録しました"
+      return
+    else
+      @tool.destroy
+      render 'new'
+      flash[:notice] = "アプリとレビューを両方とも入力してください"
+    end
+  end
+
+  private
+  def review_params
+    params.require(:tool).require(:review).permit(:review).merge(user_id: current_user.id, tool_id: @tool.id)
+  end
+
 end
--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,3 +1,4 @@
 class Tool < ActiveRecord::Base
   has_many :reviews
+  accepts_nested_attributes_for :reviews
 end
