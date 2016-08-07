614c79314436841053c6dcb2fccd0cf6bbe2b999
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-07 20:58:31 +0900

    remove column 'comment' from profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801074027) do
+ActiveRecord::Schema.define(version: 20160807115659) do
 
   create_table "photos", force: :cascade do |t|
     t.string   "image",      limit: 255
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20160801074027) do
   create_table "profiles", force: :cascade do |t|
     t.integer  "age",        limit: 4
     t.string   "job",        limit: 255
-    t.text     "comment",    limit: 65535
     t.integer  "user_id",    limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
