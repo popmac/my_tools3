875d9094e4c3b9f66845443bab3db3d3f1cf70a2
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-08 01:35:42 +0900

    add comment create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    @review = Review.find(params[:review_id])
+    Comment.create(comment_params)
+  end
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(review_id: @review.id, user_id: current_user.id)
+  end
+
 end
