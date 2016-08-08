be24621e14eb80572a782922a93f39148f7b28a2
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-08 16:25:23 +0900

    define likes#destroy action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,4 +5,10 @@ class LikesController < ApplicationController
     @review = Review.find(params[:review_id])
   end
 
+  def destroy
+    like = Like.find_by(user_id: current_user.id, review_id: params[:review_id])
+    like.destroy
+    @review = Review.find(params[:review_id])
+  end
+
 end
