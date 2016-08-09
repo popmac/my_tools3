f513a3a6f8e162590c588ac39e03ffc7109be157
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-09 16:34:31 +0900

    add avatar column to profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808064635) do
+ActiveRecord::Schema.define(version: 20160809072924) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment",    limit: 65535
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160808064635) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "introduce",  limit: 65535
+    t.string   "avatar",     limit: 255
   end
 
   create_table "reviews", force: :cascade do |t|
