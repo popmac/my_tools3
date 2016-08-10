69e1aff23558828560c3359bb50bf6e10c69ede9
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-10 17:30:46 +0900

    create 'carrierwave.rb'

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,18 @@
+CarrierWave.configure do |config|
+    config.storage = :fog
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    region: 'us-east-1'
+  }
+
+    case Rails.env
+    when 'development'
+        config.fog_directory  = 'upload-mytools'
+        config.asset_host = 'https://s3.amazonaws.com/upload-mytools'
+    when 'production'
+        config.fog_directory  = 'upload-mytools'
+        config.asset_host = 'https://s3.amazonaws.com/upload-mytools'
+    end
+end
