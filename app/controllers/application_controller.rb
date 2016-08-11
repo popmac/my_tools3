90048b1d0e90635fb77e09bdcdacc78f250af00a
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-11 18:02:09 +0900

    change key 'nickname' to 'username'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname ,:email])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname ,:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username ,:email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username ,:email])
   end
 
 end
