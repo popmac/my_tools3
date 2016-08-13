b5d33c9bc71d7f611b5bbbe1962d005fca6b1e80
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-13 14:08:00 +0900

    define users_reviews_number_ranking#index action

--- app/controllers/users_reviews_number_ranking_controller.rb
+++ app/controllers/users_reviews_number_ranking_controller.rb
@@ -1,2 +1,8 @@
 class UsersReviewsNumberRankingController < ApplicationController
+
+  def index
+    user_ids = Review.group(:user_id).order('count_user_id DESC').count(:user_id).keys
+    @users = user_ids.map { |id| User.find(id) }
+  end
+
 end
