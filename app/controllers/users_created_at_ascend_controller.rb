b0f1266f34b76fe05fe67eadf3e2e08e5cdcd325
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-09-06 20:08:00 +0900

    rails g controller users_created_at_ascend

---
+++ app/controllers/users_created_at_ascend_controller.rb
@@ -0,0 +1,2 @@
+class UsersCreatedAtAscendController < ApplicationController
+end
---
+++ test/controllers/users_created_at_ascend_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UsersCreatedAtAscendControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
