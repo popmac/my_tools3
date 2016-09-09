2982093fcf226d67d90027dfad06a8d4f21fb91f
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-09-09 21:16:51 +0900

    Fix flash message at registrations/new for deficiency of input member registration.

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,13 +3,20 @@ module DeviseHelper
   def devise_error_messages!
     return "" unless devise_error_messages?
 
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      :count => resource.errors.count,
+                      :resource => resource.class.model_name.human.downcase)
+
     html = <<-HTML
     <div class="container" id="notice">
       <div class="alert alert-dismissable alert-danger fade in">
         <button class="close" data-dismiss="alert">
           <span>&times;</span>
         </button>
-        入力情報に不備があります。もう一度確認してから入力してください。
+        #{sentence}
+        #{messages}
+        もう一度確認してから入力してください。
       </div>
     </div>
     HTML
