413d6a34d5034dcd4729a78b9004f55f8b8c0960
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-09-13 18:11:54 +0900

    define likes#index action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,9 @@
 class LikesController < ApplicationController
 
+  def index
+    @likes = current_user.likes.includes({review: [user: :profile]}, {review: :tool})
+  end
+
   def create
     @like = Like.create(user_id: current_user.id, review_id: params[:review_id])
     @review = Review.find(params[:review_id])
