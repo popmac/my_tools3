50f74d485ad2779b07b534642db24afdfb79dee6
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-09-23 18:55:49 +0900

    Update users#index and show action.
    profileが登録されていない場合の設定をした。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController
 
   def index
-    # 自分が一番上に表示されるようにしている
-    @users = [current_user, *User.where.not(id: current_user.id).includes(:profile).to_a]
     @user = User.find(current_user.id)
-    @introduce = current_user.profile.introduce
-    @photos = current_user.photos
-    @reviews = current_user.reviews.includes(:tool)
+    # profileが登録されている場合は以下のインスタンスを生成
+    unless current_user.profile.avatar == nil || current_user.profile.nickname == nil || current_user.profile.age == nil || current_user.profile.job == nil || current_user.profile.introduce == nil
+      # 自分が一番上に表示されるようにしている
+      @users = [current_user, *User.where.not(id: current_user.id).includes(:profile).to_a]
+      @introduce = current_user.profile.introduce
+      @photos = current_user.photos
+      @reviews = current_user.reviews.includes(:tool)
+    end
   end
 
   def show
+    # profileが登録されていない場合はusers/indexにリダイレクト
+    if current_user.profile.avatar == nil || current_user.profile.nickname == nil || current_user.profile.age == nil || current_user.profile.job == nil || current_user.profile.introduce == nil
+      redirect_to controller: :users, action: :index
+      return
+    end
     # users/showで表示の対象となるuserを設定
     @active_user = User.find(params[:id])
     @users = [@active_user, *User.where.not(id: @active_user.id).includes(:profile).to_a]
