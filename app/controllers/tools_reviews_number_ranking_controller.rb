7bf1a8cd0b55369d6f0e2ae4f532a88d68f893b6
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-09-23 20:48:56 +0900

    define tools_reviews_number_ranking#index action

--- app/controllers/tools_reviews_number_ranking_controller.rb
+++ app/controllers/tools_reviews_number_ranking_controller.rb
@@ -1,2 +1,6 @@
 class ToolsReviewsNumberRankingController < ApplicationController
+  def index
+    tool_ids = Review.group(:tool_id).order('count_tool_id DESC').count(:tool_id).keys
+    @tools = tool_ids.map { |id| Tool.find(id) }
+  end
 end
