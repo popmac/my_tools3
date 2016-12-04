2cb596e478da6a392fcd4986d138fe0c19d57ba9
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-04 18:45:24 +0900

    Move "include Devise::Test::ControllerHelpers" to test_helper.rb.

--- test/controllers/tools_controller_test.rb
+++ test/controllers/tools_controller_test.rb
@@ -2,8 +2,6 @@ require 'test_helper'
 
 class ToolsControllerTest < ActionController::TestCase
 
-  include Devise::Test::ControllerHelpers
-
   def setup
     @user = users(:hoge1)
     sign_in(@user)
