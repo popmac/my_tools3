7ab91e5d75a377f883ca11df8b9ccf687df5cd3f
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-04 17:57:26 +0900

    Add devise setting for sign_in at tools_controller_test.

--- test/controllers/tools_controller_test.rb
+++ test/controllers/tools_controller_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class ToolsControllerTest < ActionController::TestCase
+
+  include Devise::Test::ControllerHelpers
+
+  def setup
+    @user = users(:hoge1)
+    sign_in(@user)
+  end
+
 end
