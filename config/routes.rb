2865f05ea79b6f0f6a42c80a75c855bb5cefa105
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-04 19:23:34 +0900

    Fix users path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users do
     resources :photos
+  resources :users, only: [:index, :show] do
   end
   resources :profiles
   resources :tools do
