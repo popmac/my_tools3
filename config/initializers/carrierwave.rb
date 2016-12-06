779af8db995eaaca52040078b780dd0de3df56fe
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-06 22:07:50 +0900

    FIx carrierwave.rb.
    画像のアップロード先を、開発環境とテスト環境ではローカル、本番環境ではS3、に分けるようにした。

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,15 @@
 CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
     config.storage = :fog
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'us-east-1'
-  }
-
-    case Rails.env
-    when 'development'
-        config.fog_directory  = 'development-upload-mytools'
-        config.asset_host = 'https://s3.amazonaws.com/development-upload-mytools'
-    when 'production'
-        config.fog_directory  = 'upload-mytools'
-        config.asset_host = 'https://s3.amazonaws.com/upload-mytools'
-    end
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+      region: 'us-east-1'
+    }
+    config.fog_directory  = 'upload-mytools'
+    config.asset_host = 'https://s3.amazonaws.com/upload-mytools'
+  end
 end
