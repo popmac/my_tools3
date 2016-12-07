aa4e4f761aa869c53b1b3a5c6c1b57d220bb016e
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-07 21:23:08 +0900

    Delete unique of email column of users table. Add unique email and deleted_at of users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207120715) do
+ActiveRecord::Schema.define(version: 20161207121904) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment",    limit: 65535
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 20161207120715) do
   end
 
   add_index "users", ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email", "deleted_at"], name: "index_users_on_email_and_deleted_at", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
