9e03df802b88474c1851fe73991d1df7f56e1c16
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-07 15:33:01 +0900

    Add seed of user and profile.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create!(username:  "hoge1",
+                    email: "hoge1@hoge.com",
+                    password:              "aaaa1111",
+                    password_confirmation: "aaaa1111")
+
+image_path = File.join(Rails.root, "test/fixtures/images/profile-1.jpg")
+
+user.profile.update!(age: 29,
+                     job: "Webマーケター",
+                     introduce: "よろしくお願いします！",
+                     avatar: File.new(image_path),
+                     nickname: "まっちゃん",
+                     user_id: 1)
+
+19.times do |n|
+  username  = "hoge#{n+2}"
+  email = "hoge#{n+2}@hoge.com"
+  user = User.create!(username: username,
+                      email: email,
+                      password:              "aaaa1111",
+                      password_confirmation: "aaaa1111")
+  image_path = File.join(Rails.root, "test/fixtures/images/profile-#{n+2}.jpg")
+  user.profile.update!(age: "#{n+30}",
+                       job: "Webマーケター",
+                       introduce: "よろしくお願いします！",
+                       avatar: File.new(image_path),
+                       nickname: "まっちゃん",
+                       user_id: "#{n+2}")
+end
