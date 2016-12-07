abf7b9f6155e9b0357bd99e6a561f1fd8095d8ea
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-07 21:18:17 +0900

    Delete unique of email by overriding devise setting.

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,20 @@ class User < ActiveRecord::Base
     build_profile
     true
   end
+
+  # deviseの設定をオーバーライドして、emailのユニーク制約を外す
+  def self.included(base)
+    base.extend ClassMethods
+    assert_validations_api!(base)
+
+    base.class_eval do
+      validates_presence_of   :email, if: :email_required?
+      # validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
+      validates_format_of     :email, with: email_regexp, allow_blank: true, if: :email_changed?
+
+      validates_presence_of     :password, if: :password_required?
+      validates_confirmation_of :password, if: :password_required?
+      validates_length_of       :password, within: password_length, allow_blank: true
+    end
+  end
 end
