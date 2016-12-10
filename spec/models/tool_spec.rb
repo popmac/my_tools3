202e14148233ee7ceee16cf0e7480ed76095a54b
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-10 23:49:18 +0900

    Add test 'tool is valid with a name'.

--- spec/models/tool_spec.rb
+++ spec/models/tool_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Tool, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # nameがあれば有効な状態であること
+  it "is valid with a name" do
+    tool = Tool.new(
+      name: 'Atom',
+    )
+    expect(tool).to be_valid
+  end
 end
