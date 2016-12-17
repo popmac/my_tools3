d0772fabaaf556002a711c206a4a9869172e98f1
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-17 13:33:59 +0900

    Add user and profile factory.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,17 @@
 FactoryGirl.define do
-  factory :user do
+  factory :user, class: User do
+    username 'test_user'
+    email 'test@example.com'
+    password 'password'
+    profile
+  end
+  FactoryGirl.modify do
+    factory :profile, class: Profile do
+      age '29'
+      job 'web'
+      introduce 'hello'
+      avatar 'hoge.png'
+      nickname 'mats'
+    end
   end
 end
