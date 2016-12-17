5f88ee6dc5659ad51d690a43a48ceaa84846008d
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-17 13:28:34 +0900

    Apply spec/support/controller_macros.rb to RSpec.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'devise'
+require 'support/controller_macros'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -59,4 +60,5 @@ end
 
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
 end
