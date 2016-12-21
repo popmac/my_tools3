f0f63d99d1e9399348c2c5b2349146e1ba54bb14
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-21 21:38:03 +0900

    Refactor rails _helper.rb.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,9 +56,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-end
 
-RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
 end
