3e1c70eff7ced7580a2eebb98ffff311cf781080
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-01-18 12:02:34 +0900

    Add factory for review.

--- spec/factories/tools.rb
+++ spec/factories/tools.rb
@@ -5,4 +5,13 @@ FactoryGirl.define do
       name nil
     end
   end
+  FactoryGirl.modify do
+    factory :review, class: Review do
+      review '最高'
+      rate 80
+      likes_count 2
+      tool_id 1
+      user_id 1
+    end
+  end
 end
